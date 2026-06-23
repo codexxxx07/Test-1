27df76e7e04355ee01d2264d0f4c32d19d1facc5
codexxxx07 <ikrish21052007@gmail.com>
2026-06-24 04:28:08 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-red-500 text-center">
+      <h1 className="m-8 p-8 text-3xl">Huii !!!</h1>
+    </div>
+  );
+}
+
+export default App;
